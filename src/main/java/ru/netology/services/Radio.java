81b7fecfcfd336b71cdccbd14f9d02b505fa0c06
Dmitry Zolotov <zolotov_dmitry@mail.ru>
2024-03-21 12:02:08 +0300

    change Radio.java

--- src/main/java/ru/netology/services/Radio.java
+++ src/main/java/ru/netology/services/Radio.java
@@ -2,79 +2,81 @@ package ru.netology.services;
 
 public class Radio {
 
-    public int currentStation;
+    private int currentStation;
+    private int currentVolume;
 
     public int getCurrentStation() {
         return currentStation;
     }
 
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
     public void setCurrentStation(int newCurrentStation) {
         if (newCurrentStation < 0) {
-            currentStation = 0;
+            setCurrentStation(0);
             return;
         }
         if (newCurrentStation > 9) {
-            currentStation = 9;
+            setCurrentStation(9);
             return;
         }
         currentStation = newCurrentStation;
     }
 
-    public void next() {
-        if ((currentStation + 1) < 9) {
-            currentStation = currentStation + 1;
-        }
-        if ((currentStation + 1) > 9) {
-            currentStation = 0;
-        }
-        return;
-    }
-
-    public void prev() {
-        if (currentStation > 0) {
-            currentStation = currentStation - 1;
-        }
-        if ((currentStation - 1) < 0) {
-            currentStation = 9;
-        }
-        return;
-    }
-
-
-    public int currentVolume;
-
-    public int getCurrentVolume() {
-        return currentVolume;
-    }
-
     public void setCurrentVolume(int newCurrentVolume) {
         if (newCurrentVolume < 0) {
-            currentVolume = 0;
+            setCurrentVolume(0);
             return;
         }
         if (newCurrentVolume > 100) {
-            currentVolume = 100;
+            setCurrentVolume(100);
             return;
         }
         currentVolume = newCurrentVolume;
     }
 
+    public void next() {
+        int nextStation = currentStation +1;
+        if (nextStation < 9) {
+            setCurrentStation(nextStation);
+        }
+        if (nextStation > 9) {
+            setCurrentStation(0);
+        }
+        return;
+    }
+
+    public void prev() {
+        int prevStation = currentStation - 1;
+        if (currentStation > 0) {
+            setCurrentStation(prevStation);
+        }
+        if (prevStation < 0) {
+            setCurrentStation(9);
+        }
+        return;
+    }
+
     public void increaseVolume() {
+        int nextVolume = currentVolume + 1;
         if (currentVolume == 100) {
-            currentVolume = 100;
+            setCurrentVolume(100);
         }
         if (currentVolume < 100) {
-            currentVolume = currentVolume + 1;
+            setCurrentVolume(nextVolume);
         }
         return;
     }
 
     public void decreaseVolume() {
+        int prevVolume = currentVolume - 1;
         if (currentVolume <= 0) {
-            currentVolume = 0;
+            setCurrentVolume(0);
         }
         if (currentVolume > 0) {
-            currentVolume = currentVolume - 1;
+            setCurrentVolume(prevVolume);
         }
         return;
     }
